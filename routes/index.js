696f34b4e928e354bec1e616489a63178a2cdc52
Mohammad Harris <harris@tmrnd.com.my>
2018-09-05 14:48:19 +0800

    Introduce the assignment mistake in the condition checking

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// List of API
+router.get ('/test1', cbGetTest1);
+
+
+// Implementation
+function cbGetTest1 (req, res) {
+  let a = 5;
+
+  if(a = 10) {
+    console.log (`a = ${a}`);
+  }
+  else {
+    console.log ("Nothing to show");
+  }
+
+  res.send ({status: "OK"});
+}
+
 module.exports = router;
