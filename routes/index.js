5b6b4cff55ad78991157e1c04f312398fb8cc941
Mohammad Harris <harris@tmrnd.com.my>
2018-09-06 09:28:23 +0800

    introduce sql injection and url redirect

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,10 @@ router.get ('/test2', cbGetTest2);
 // Implementation for callback
 function cbGetTest2 (req, res) {
 
+    var query = 'SELECT * table1 WHERE id = ${req.body.id}';
 
-    res.send ({status: "DONE"});
+//    res.send ({status: "DONE"});
+    res.redirect (req.body.url);
 }
 
 module.exports = router;
